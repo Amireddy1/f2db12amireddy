560191595a5069caef670c88dbc568f3f7fb2404
Amireddy1 <amireddy.shruthi@gmail.com>
2022-11-09 18:03:47 -0600

    final commiting

--- controllers/boats.js
+++ controllers/boats.js
@@ -19,8 +19,26 @@ exports.boats_detail = function(req, res) {
 }; 
  
 // Handle Boats create on POST. 
-exports.boats_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: boats create POST'); 
+exports.boats_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Boats(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    //{"costume_type":"goat", "cost":12, "size":"large"} 
+    //{"BoatType":"Sail powered boats", "BoatsCost":5000, "Capacity":"420", "Hull":"V-Shaped Hulls"}
+    document.BoatType = req.body.BoatType; 
+    document.BoatsCost = req.body.BoatsCost; 
+    document.Capacity = req.body.Capacity; 
+    document.Hull = req.body.Hull;
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Boats delete form on DELETE. 
